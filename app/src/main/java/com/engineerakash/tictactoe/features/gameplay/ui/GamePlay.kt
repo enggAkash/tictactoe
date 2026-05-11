6c1930f4bd31680a61154daac8624f5df9b07662
Akash Kumar <cse.akashkumar@gmail.com>
2026-05-11 18:42:59 +0530

    implement win detection logic and update score counters in gameplay screen

--- app/src/main/java/com/engineerakash/tictactoe/features/gameplay/ui/GamePlay.kt
+++ app/src/main/java/com/engineerakash/tictactoe/features/gameplay/ui/GamePlay.kt
@@ -1,5 +1,6 @@
 package com.engineerakash.tictactoe.features.gameplay.ui
 
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -31,6 +32,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -70,8 +72,10 @@ fun GamePlay(gameType: String, onBackPressed: () -> Unit) {
     val (p1IconResource, p2IconResource, p1Index) = getP1P2IconAndIndex()
     val p1Icon = painterResource(p1IconResource)
     val p2Icon = painterResource(p2IconResource)
-    val p1WinCounter by rememberSaveable { mutableIntStateOf(0) }
-    val p2WinCounter by rememberSaveable { mutableIntStateOf(0) }
+    val p1WinCounter: MutableState<Int> by rememberSaveable { mutableStateOf(mutableIntStateOf(0)) }
+    val p2WinCounter: MutableState<Int> by rememberSaveable { mutableStateOf(mutableIntStateOf(0)) }
+
+    val context = LocalContext.current
 
 
     Scaffold { innerPadding ->
@@ -139,7 +143,35 @@ fun GamePlay(gameType: String, onBackPressed: () -> Unit) {
                     boardMatrixValue[i][j].value = 1 - p1Index
                 }
 
-                // TODO Check who wins
+                checkWhoWins(
+                    boardMatrixValue,
+                    p1IconResource,
+                    p2IconResource,
+                    p1Index,
+                    p1WinCounter,
+                    p2WinCounter,
+                    whoWins = { indexOfWinner ->
+
+                        if (indexOfWinner == -1) {
+                            // It's a draw
+                            Toast.makeText(context, "It's a draw!", Toast.LENGTH_LONG)
+                                .show()
+
+                        } else if (p1Index == indexOfWinner) {
+                            // P1 wins
+                            p1WinCounter.value++
+                            Toast.makeText(context, "Player 1 wins", Toast.LENGTH_LONG)
+                                .show()
+
+                        } else {
+                            // P2 wins
+                            p2WinCounter.value++
+
+                            Toast.makeText(context, "Player 2 wins", Toast.LENGTH_LONG)
+                                .show()
+                        }
+                    }
+                )
 
                 p1Turn = !p1Turn
             }
@@ -147,88 +179,6 @@ fun GamePlay(gameType: String, onBackPressed: () -> Unit) {
         }
     }
 
-}
-
-@Composable
-private fun ScoreCounter(
-    p1Icon: Painter,
-    p2Icon: Painter,
-    p1WinCounter: Int,
-    p2WinCounter: Int,
-) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceEvenly
-    ) {
-
-        Card(
-            colors = CardDefaults.cardColors(LightModeDarkModeColor),
-            modifier = Modifier.padding(10.dp)
-        ) {
-            Column(
-                modifier = Modifier.padding(5.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-
-                Icon(
-                    p1Icon,
-                    tint = null,
-                    modifier = Modifier
-                        .size(50.dp)
-                        .padding(5.dp),
-                    contentDescription = "P1 Icon",
-                )
-
-                Row(
-                    horizontalArrangement = Arrangement.SpaceEvenly,
-                ) {
-                    Text("You:", color = Color.White, fontWeight = FontWeight.Bold)
-                    Spacer(modifier = Modifier.size(5.dp))
-                    Text(
-                        text = p1WinCounter.toString(),
-                        color = Color.White,
-                        fontWeight = FontWeight.Bold
-                    )
-                }
-
-            }
-        }
-
-        Card(
-            colors = CardDefaults.cardColors(LightModeDarkModeColor),
-            modifier = Modifier.padding(10.dp)
-        ) {
-            Column(
-                modifier = Modifier.padding(5.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-
-                Icon(
-                    p2Icon,
-                    tint = null,
-                    modifier = Modifier
-                        .size(50.dp)
-                        .padding(5.dp),
-                    contentDescription = "P2 Icon",
-                )
-
-                Row(
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                ) {
-                    Text("Bot:", color = Color.White, fontWeight = FontWeight.Bold)
-                    Spacer(modifier = Modifier.size(5.dp))
-                    Text(
-                        text = p2WinCounter.toString(),
-                        color = Color.White,
-                        fontWeight = FontWeight.Bold
-                    )
-                }
-
-            }
-        }
-
-    }
 }
 
 @Composable
@@ -333,6 +283,172 @@ fun getP1P2IconAndIndex(): Triple<Int, Int, Int> {
     val p1Index = Random.nextDouble().roundToInt()
 
     return Triple(icons[p1Index], icons[1 - p1Index], p1Index)
+}
+
+@Composable
+private fun ScoreCounter(
+    p1Icon: Painter,
+    p2Icon: Painter,
+    p1WinCounter: MutableState<Int>,
+    p2WinCounter: MutableState<Int>,
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+
+        Card(
+            colors = CardDefaults.cardColors(LightModeDarkModeColor),
+            modifier = Modifier.padding(10.dp)
+        ) {
+            Column(
+                modifier = Modifier.padding(5.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Icon(
+                    p1Icon,
+                    tint = null,
+                    modifier = Modifier
+                        .size(50.dp)
+                        .padding(5.dp),
+                    contentDescription = "P1 Icon",
+                )
+
+                Row(
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                ) {
+                    Text("You:", color = Color.White, fontWeight = FontWeight.Bold)
+                    Spacer(modifier = Modifier.size(5.dp))
+                    Text(
+                        text = p1WinCounter.value.toString(),
+                        color = Color.White,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+
+            }
+        }
+
+        Card(
+            colors = CardDefaults.cardColors(LightModeDarkModeColor),
+            modifier = Modifier.padding(10.dp)
+        ) {
+            Column(
+                modifier = Modifier.padding(5.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Icon(
+                    p2Icon,
+                    tint = null,
+                    modifier = Modifier
+                        .size(50.dp)
+                        .padding(5.dp),
+                    contentDescription = "P2 Icon",
+                )
+
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                ) {
+                    Text("Bot:", color = Color.White, fontWeight = FontWeight.Bold)
+                    Spacer(modifier = Modifier.size(5.dp))
+                    Text(
+                        text = p2WinCounter.value.toString(),
+                        color = Color.White,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+
+            }
+        }
+
+    }
+}
+
+fun checkWhoWins(
+    boardMatrixValue: Array<Array<MutableState<Int>>>,
+    p1IconResource: Int,
+    p2IconResource: Int,
+    p1Index: Int,
+    p1WinCounter: MutableState<Int>,
+    p2WinCounter: MutableState<Int>,
+    whoWins: (Int) -> Unit
+) {
+    val isAllBoxesAreFilled = isAllBoxesAreFilled(boardMatrixValue)
+
+    val indexOfWinner = indexOfWhoWins(boardMatrixValue)
+
+    if (indexOfWinner == -1) {
+        // currently no one wins
+    } else if (p1Index == indexOfWinner) {
+        // P1 wins
+        whoWins(indexOfWinner)
+    } else {
+        // P2 wins
+        whoWins(indexOfWinner)
+    }
+
+    if (!isAllBoxesAreFilled) {
+        return
+    }
+
+    // It's a Draw
+    whoWins(-1)
+}
+
+/**
+ * 0 -> if Zero(0) wins
+ * 1 -> if Kata(x) wins
+ * -1 -> if it's a draw
+ */
+fun indexOfWhoWins(boardMatrixValue: Array<Array<MutableState<Int>>>): Int {
+
+    /**
+     * [00] [01] [02]
+     * [10] [11] [12]
+     * [20] [21] [22]
+     */
+
+    /*Horizontal Matching*/
+    if (boardMatrixValue[0][0].value == boardMatrixValue[0][1].value && boardMatrixValue[0][1].value == boardMatrixValue[0][2].value) {
+        return boardMatrixValue[0][0].value
+    } else if (boardMatrixValue[1][0].value == boardMatrixValue[1][1].value && boardMatrixValue[1][1].value == boardMatrixValue[1][2].value) {
+        return boardMatrixValue[1][0].value
+    } else if (boardMatrixValue[2][0].value == boardMatrixValue[2][1].value && boardMatrixValue[2][1].value == boardMatrixValue[2][2].value) {
+        return boardMatrixValue[2][0].value
+    }
+
+    /*Vertical Matching*/
+    if (boardMatrixValue[0][0].value == boardMatrixValue[1][0].value && boardMatrixValue[1][0].value == boardMatrixValue[2][0].value) {
+        return boardMatrixValue[0][0].value
+    } else if (boardMatrixValue[0][1].value == boardMatrixValue[1][1].value && boardMatrixValue[1][1].value == boardMatrixValue[2][1].value) {
+        return boardMatrixValue[0][1].value
+    } else if (boardMatrixValue[0][2].value == boardMatrixValue[1][2].value && boardMatrixValue[1][2].value == boardMatrixValue[2][2].value) {
+        return boardMatrixValue[0][2].value
+    }
+
+    /*Diagonal Matching*/
+    if (boardMatrixValue[0][0].value == boardMatrixValue[1][1].value && boardMatrixValue[1][1].value == boardMatrixValue[2][2].value) {
+        return boardMatrixValue[0][0].value
+    } else if (boardMatrixValue[0][2].value == boardMatrixValue[1][1].value && boardMatrixValue[1][1].value == boardMatrixValue[2][0].value) {
+        return boardMatrixValue[0][2].value
+    }
+
+    return -1
+}
+
+fun isAllBoxesAreFilled(boardMatrixValue: Array<Array<MutableState<Int>>>): Boolean {
+    for (i in 0 until boardMatrixValue.size) {
+        for (j in 0 until boardMatrixValue[i].size) {
+            if (boardMatrixValue[i][j].value == -1) {
+                return false
+            }
+        }
+    }
+
+    return true
 }
 
 @Preview
